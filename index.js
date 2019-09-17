8210582492875f2f4c06945fdc8588e3a8b3609f
Mahammad <mahammad.c@productivet.com>
2019-09-17 11:24:42 +0530

    push

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const MongoClient = require('mongodb').MongoClient
 const url = 'mongodb://localhost:27017'
 
 // Database Name
-const dbName = 'myproject'
+const dbName = 'test'
 
 // Use connect method to connect to the server
 MongoClient.connect(url, function (err, client) {
